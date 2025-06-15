eb54a841163ac6c2db6b41cd85c1a57080a62c64
yoohans <92737938+el-quero@users.noreply.github.com>
2025-06-15 08:26:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -19,11 +19,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // PostgreSQL pool setup
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Supabase butuh SSL
+  },
 });
 
 // Buat tabel jika belum ada
